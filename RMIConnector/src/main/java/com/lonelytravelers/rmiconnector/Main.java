f1d925d1b389c8501afb976e510ef056c953c0ec
davi7725 <davi7816@edu.eal.dk>
2019-12-13 18:17:15 +0100

    Added necessary code to be able to call the 2 needed methods from the Legacy program

--- RMIConnector/src/main/java/com/lonelytravelers/rmiconnector/Main.java
+++ RMIConnector/src/main/java/com/lonelytravelers/rmiconnector/Main.java
@@ -7,6 +7,12 @@ package com.lonelytravelers.rmiconnector;
 
 import DTOs.AvailabilityDetails;
 import DTOs.CarDetails;
+import DTOs.CarTypeDetails;
+import DTOs.DriverDetails;
+import DTOs.HotelDetails;
+import DTOs.Identifiers.StationIdentifier;
+import DTOs.NewBookingDetails;
+import interfaces.IBooking;
 import interfaces.ICars;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
@@ -15,6 +21,7 @@ import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -29,16 +36,47 @@ public class Main {
     public Main() throws RemoteException {
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         try {
+            /*SETUP VARS FOR NEW BOOKING*/
+            NewBookingDetails newBookingDetails = new NewBookingDetails();
+            CarTypeDetails carDetails = new CarTypeDetails("1", "A", 3, 992.10);
+            CarDetails car = new CarDetails("5309118446", carDetails);
+            DriverDetails driver = new DriverDetails("1", "Gretta Thake", new SimpleDateFormat("yyyy-MM-dd").parse("1966-10-18"), "22-9484786");
+            
+            newBookingDetails.setPickUpTime(new SimpleDateFormat("yyyy-MM-dd").parse("2019-04-15"));
+            newBookingDetails.setDeliveryTime(new SimpleDateFormat("yyyy-MM-dd").parse("2019-04-20"));
+            newBookingDetails.setPickUpPlace(new HotelDetails("24", "","",0,null,""));
+            newBookingDetails.setDeliveryPlace(new HotelDetails("24", "","",0,null,""));
+            newBookingDetails.setDriver(driver);
+            newBookingDetails.setCar(car);
+            
+            
+            /*SETUP VARS FOR AVAILABL CARS*/
+            AvailabilityDetails availabilityDetails = new AvailabilityDetails(
+                        new SimpleDateFormat("yyyy-MM-dd").parse("2019-04-15"), 
+                        new SimpleDateFormat("yyyy-MM-dd").parse("2019-04-20"), 
+                        new HotelDetails("24", "","",0,null,""));
+            
+            /*CONNECTING BOOKING CLIENT RMI*/
+            IBooking bookingClient = (IBooking) Naming.lookup("//localhost/Booking");
+            
+            /*CONNECTING CAR CLIENT RMI*/
             ICars carClient = (ICars) Naming.lookup("//localhost/Cars");
-            AvailabilityDetails availabilityDetails = new AvailabilityDetails(new SimpleDateFormat("yyyy-MM-dd").parse("2019-04-15"), new SimpleDateFormat("yyyy-MM-dd").parse("2019-04-20"), new DTOs.HotelDetails("24", "","",0,null,""));
+            
+            /*CREATE ADD BOOKING REQUEST AND PROCESSING*/
+            bookingClient.addNewBooking(newBookingDetails);
+            
+            
+            /*CREATE AND PROCESS CAR DATA*/
             List<CarDetails> list = carClient.getAvailableCars(availabilityDetails);
             
             for(CarDetails carD : list)
             {
                 System.out.println(carD.getLicensePlate());
             }
+            
+            
         } catch (NotBoundException | MalformedURLException | RemoteException | ParseException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
