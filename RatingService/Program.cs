473b699637e089694498762e0e56d96264d06abc
Elitsa <elit0451@edu.eal.dk>
2019-12-15 19:39:38 +0100

    Adding implementation for Rating service. Added connection to DB by AppDB.cs. Added DB creating and its tables in DBSCHEMA. Added MessageGateway to communicating with RabbitMQ. Adding Rating and RatingQuery models. Added interface IJSONConvertable for easily converting string to JSON.

---
+++ RatingService/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RatingService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MessageGateway.ReceiveRating();
+            Console.WriteLine("Running");
+            while (true) { }
+        }
+    }
+}
