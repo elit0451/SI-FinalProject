0eb09288dd7d045f68fa4cebac9860ff95873ff2
Elitsa <elit0451@edu.eal.dk>
2019-12-15 12:13:07 +0100

    Removing unused variable and unused ctor

--- RMIConnector/src/main/java/com/lonelytravelers/rmiconnector/Main.java
+++ RMIConnector/src/main/java/com/lonelytravelers/rmiconnector/Main.java
@@ -1,16 +1,10 @@
 package com.lonelytravelers.rmiconnector;
 
 import java.io.IOException;
-import java.rmi.RemoteException;
-import java.rmi.registry.Registry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-    public static Registry registry;
-
-    public Main() throws RemoteException {
-    }
 
     public static void main(String[] args) {
         try {
