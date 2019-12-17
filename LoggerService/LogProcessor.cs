d6d00b2d3fca181c7bed744ca9f9ba6ee5cc2f54
David Alves <davi7816@edu.eal.dk>
2019-12-17 01:00:08 +0100

    Removed the static SteamWritter as it was keeping the file locked to the program and wouldn't allow to view the file after closing. The current approach allows the file to be seen at any time.

--- LoggerService/LogProcessor.cs
+++ LoggerService/LogProcessor.cs
@@ -6,19 +6,12 @@ namespace LoggerService
 {
     public static class LogProcessor
     {
-        private static StreamWriter writer;
-
-        static LogProcessor()
-        {
-            writer = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "log.txt", true);
-        }
-
-
         public static void Process(Log log)
         {
             if (log.Level == LogLevel.CRITICAL)
                 Console.WriteLine("[{0}] - {1} - {2}", log.Level, log.Service, log.Message);
-
+                
+            using(StreamWriter writer = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "log.txt", true))
                 writer.WriteLine("[{0}] - {1} - {2}", log.Level, log.Service, log.Message);
         }
     }
