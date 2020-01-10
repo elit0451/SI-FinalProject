eb451b721ec804826bdfa5ca46c7cdf38ede70ef
Elitsa <elit0451@edu.eal.dk>
2020-01-10 16:07:30 +0100

    Refactored the RMIConnector with missing message attributes

--- RMIConnector/src/main/java/com/lonelytravelers/rmiconnector/CommandRouter.java
+++ RMIConnector/src/main/java/com/lonelytravelers/rmiconnector/CommandRouter.java
@@ -1,33 +1,43 @@
 package com.lonelytravelers.rmiconnector;
 
 import java.io.IOException;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class CommandRouter {
-    public static void Route(String message) throws IOException
-        {
-            JSONObject receivedObj = new JSONObject(message);
-            String command = receivedObj.getString("Command");
-            switch (command)
-            {
-                case "getAvailableCars":
-                    String dateFrom = receivedObj.getString("From");
-                    String dateTo = receivedObj.getString("To");
-                    String stationId = receivedObj.getString("StationId");
-                    
-                    String availableJSON = Connector.getAvailableCars(dateFrom, dateTo, stationId);
-                    MessageGateway.SendAvailableCars(availableJSON);
-                    break;
-                case "addBooking":
-                    String pickUpTime = receivedObj.getString("PickUpTime");
-                    String deliveryTime = receivedObj.getString("DeliveryTime");
-                    String pickUpPlace = receivedObj.getString("PickUpPlace");
-                    String deliveryPlace = receivedObj.getString("DeliveryPlace");
-                    String driver = receivedObj.getString("Driver");
-                    String car = receivedObj.getString("Car");
-                    
-                    Connector.addNewBooking(pickUpTime, deliveryTime, pickUpPlace, deliveryPlace, driver, car);
-                    break;
+    public static void Route(String message) throws IOException {
+
+        JSONObject receivedObj = new JSONObject(message);
+        String command = receivedObj.getString("Command");
+
+        switch (command) {
+            case "getAvailableCars":
+                String requestId = receivedObj.getString("RequestId");
+                String dateFrom = receivedObj.getString("From");
+                String dateTo = receivedObj.getString("To");
+                String stationId = receivedObj.getString("StationId");
+
+                String availableJSON = Connector.getAvailableCars(dateFrom, dateTo, stationId);
+                JSONArray availableCars = new JSONArray(availableJSON);
+                
+                JSONObject reply = new JSONObject();
+                reply.put("Command", "carFound");
+                reply.put("RequestId", requestId);
+                reply.put("Cars", availableCars);
+
+                MessageGateway.SendAvailableCars(reply.toString());
+                break;
+            case "addBooking":
+                String pickUpTime = receivedObj.getString("PickUpTime");
+                String deliveryTime = receivedObj.getString("DeliveryTime");
+                String pickUpPlace = receivedObj.getString("PickUpPlace");
+                String deliveryPlace = receivedObj.getString("DeliveryPlace");
+                String driver = receivedObj.getString("Driver");
+                String car = receivedObj.getString("Car");
+
+                Connector.addNewBooking(pickUpTime, deliveryTime, pickUpPlace, deliveryPlace, driver, car);
+                break;
             }
-        }
+    }
 }
